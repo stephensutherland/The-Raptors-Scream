d855b019f8585e7be04eff88fd4258b67eaf58fc
Stephen <stephensutherland@users.noreply.github.com>
2026-08-08 21:06:50 -0400

    Update main.jsx

--- web-dashboard/src/main.jsx
+++ web-dashboard/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/app/OnboardingFlow.jsx'
-import "tailwindcss/tailwind.css" // <-- THIS LINE IS CRITICAL
+// import "tailwindcss/tailwind.css" // <-- THIS LINE IS CRITICAL or not 
+import "./index.css"  // <--- Change this line to import your new CSS file!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
