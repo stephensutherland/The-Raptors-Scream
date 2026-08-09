12eb19083caeba67b9a369422affa34cd70cf7d3
Stephen <stephensutherland@users.noreply.github.com>
2026-08-08 20:26:33 -0400

    Update main.jsx

--- web-dashboard/src/main.jsx
+++ web-dashboard/src/main.jsx
@@ -1,1 +1,10 @@
-import App from './pages/app/OnboardingFlow.jsx'; ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './pages/app/OnboardingFlow.jsx'
+import "tailwindcss/tailwind.css" // <-- THIS LINE IS CRITICAL
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
